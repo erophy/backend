f7d47fc3e4e6a000440d03dd18c30370284a89a2
erophy <98341118+erophy@users.noreply.github.com>
2022-08-30 23:19:20 +0300

    Update main.go

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"github.com/golang-jwt/jwt"
+	"time"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.POST("/save", save)
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func run() error {
-	fmt.Println("Hello, World")
-	return nil
+func save(c echo.Context) error {
+	// Get email and password
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+	return c.String(http.StatusOK, "email:"+email+", password:"+password)
 }
+
+type TokenClaims struct {
+	expiresAt int64
+	token string
+	claims string
+	Issuer string
+	jwt.StandardClaims
+
+}
+
+type JWTService interface {
+	GenerateAccessToken(payload TokenClaims, jwtSecret []byte) (string, error)
+}
+
+type jwtService struct{}
+
+func (s jwtService) GenerateAccessToken(payload TokenClaims, jwtSecret []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	expiresAt:=time.Now().UTC().Add(60*time.Hour).Unix()
+StandardClaims: jwt.StandardClaims{
+	ExpiresAt: expiresAt,
+	Issuer := <email>,
+}
+}
+signedToken, err := token.SignedString(jwtSecret){
+if err != nil {
+return "", err
+}
+
+return signedToken, nil
+}
+
+func NewJWTService() JWTService {
+	return &jwtService{}
+}
+
+
